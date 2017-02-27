7358e5ce3b75b293ee81ac4d0a173ae2f69e307e
dronly <dronly@outlook.com>
2017-02-28 01:54:32 +0800

    添加邮件验证

--- app/models.py
+++ app/models.py
@@ -1,6 +1,8 @@
-from . import db, login_manager
 from werkzeug.security import generate_password_hash, check_password_hash
+from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
+from flask import current_app
 from flask_login import UserMixin
+from . import db, login_manager
 
 
 class Role(db.Model):
@@ -20,6 +22,7 @@ class User(UserMixin, db.Model):
     username = db.Column(db.String(64), unique=True, index=True)
     role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
     password_hash = db.Column(db.String(128))
+    confirmed = db.Column(db.Boolean, default=False)
 
     @property
     def password(self):
@@ -31,6 +34,22 @@ class User(UserMixin, db.Model):
 
     def verify_password(self, password):
         return check_password_hash(self.password_hash, password)
+
+    def generate_confirmation_token(self, expriation=3600):
+        s = Serializer(current_app.config['SECRET_KEY'], expriation)
+        return s.dumps({'confim': self.id})
+
+    def confirm(self, token):
+        s = Serializer(current_app.config['SECRET_KEY'])
+        try:
+            data = s.loads(token)
+        except:
+            return False
+        if data.get('confim') != self.id:
+            return False
+        self.confirmed = True
+        db.session.add(self)
+        return True
 
     def __repr__(self):
         return '<User %r>' % self.username
