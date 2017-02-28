cffb9692b5948c51c39af7a6aceb8f0d03f3ff9b
dronly <dronly@outlook.com>
2017-02-28 20:31:59 +0800

    添加查找密码功能,以及User单元测试

--- app/models.py
+++ app/models.py
@@ -35,8 +35,8 @@ class User(UserMixin, db.Model):
     def verify_password(self, password):
         return check_password_hash(self.password_hash, password)
 
-    def generate_confirmation_token(self, expriation=3600):
-        s = Serializer(current_app.config['SECRET_KEY'], expriation)
+    def generate_confirmation_token(self, expiration=3600):
+        s = Serializer(current_app.config['SECRET_KEY'], expiration)
         return s.dumps({'confim': self.id})
 
     def confirm(self, token):
@@ -48,6 +48,22 @@ class User(UserMixin, db.Model):
         if data.get('confim') != self.id:
             return False
         self.confirmed = True
+        db.session.add(self)
+        return True
+
+    def generate_reset_token(self, expiration=3600):
+        s = Serializer(current_app.config['SECRET_KEY'], expiration)
+        return s.dumps({'reset': self.id})
+
+    def reset_password(self, token, new_password):
+        s = Serializer(current_app.config['SECRET_KEY'])
+        try:
+            data = s.loads(token)
+        except:
+            return False
+        if data.get('reset') != self.id:
+            return False
+        self.password = new_password
         db.session.add(self)
         return True
 
