036ea668540fad1f989ae89f0f606cd5f9f973ee
dronly <dronly@outlook.com>
2017-03-12 23:17:03 +0800

    添加用户编辑个人信息页面

--- app/main/views.py
+++ app/main/views.py
@@ -1,6 +1,9 @@
-from flask import render_template
+from flask import render_template, flash, redirect, url_for
+from flask_login import current_user
 from . import main
+from .. import db
 from ..models import User
+from forms import EditProfileForm
 
 
 @main.route('/', methods=['GET', 'POST'])
@@ -12,3 +15,20 @@ def index():
 def user(username):
     user = User.query.filter_by(username=username).first_or_404()
     return render_template('user.html', user=user)
+
+
+@main.route('/edit-profile', methods=['GET', 'POST'])
+def edit_profile():
+    form = EditProfileForm()
+    if form.validate_on_submit():
+        current_user.name = form.name.data
+        current_user.location = form.location.data
+        current_user.about_me = form.about_me.data
+        db.session.add(current_user)
+        flash('Your profile has been updated.')
+        return redirect(url_for('.user', username=current_user.username))
+    form.name.data = current_user.name
+    form.location.data = current_user.location
+    form.about_me.data = current_user.about_me
+    return render_template('edit_profile.html', form=form)
+
